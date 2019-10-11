e5fe42be82900464a984f94863766029084917f5
waqwb <waqwb@163.com>
2019-10-11 14:51:05 +0800

    code生成

---
+++ naan1993-guns-v3.2-final/guns/guns-admin/src/main/java/com/stylefeng/guns/modular/housemanager/service/ITblHouseService.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.modular.housemanager.service;
+
+import com.stylefeng.guns.modular.system.model.TblHouse;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Kerouac123
+ * @since 2019-10-11
+ */
+public interface ITblHouseService extends IService<TblHouse> {
+
+}
---
+++ naan1993-guns-v3.2-final/guns/guns-admin/src/main/java/com/stylefeng/guns/modular/system/dao/TblHouseMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.modular.system.dao;
+
+import com.stylefeng.guns.modular.system.model.TblHouse;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Kerouac123
+ * @since 2019-10-11
+ */
+public interface TblHouseMapper extends BaseMapper<TblHouse> {
+
+}
